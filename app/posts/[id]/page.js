7bc1f8fc8f5f5652f7b52d9842138c909ec8a49a
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-08-17 23:52:19 -0300

    someStuffInThePost

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,4 +1,5 @@
 import Postconnection from "@/app/postConnections/page"
+import styles from './id.module.css'
 
 export default async function Id({params}) {
 
@@ -8,15 +9,47 @@ export default async function Id({params}) {
     
   return(
 
-    <>
+    <>  
 
+ 
     {console.log(params.id)}
       {posts.filter((name) => name.node.title == `${params.id.charAt(0).toUpperCase() + params.id.slice(1)}`    ).map((post) => (
+        <section>
+        <section className={`${styles.containerImgId}`}>
+       <img className={`${styles.imgPostId}`} src={post.node.featureImage.url}></img>
+       </section>
 
-       <h1> {post.node.title}</h1>
+
+       
+     
+       <section style={{display:'flex',justifyContent:'center', marginTop: '20px' }}> <span className={`${styles.borderId}`}></span></section>
+     
+       
+     
+       
+        <section className={`${styles.containerTextId}`}>
+
+        <section className={`${styles.borderContainerId}`}>
+          
+          <section className={`${styles.containerTitleId}`}>
+        <h1 className={`${styles.titleId}`}>{post.node.title}</h1>
+        </section>
+        <section className={`${styles.containerPId}`}>
+       <p className={`${styles.pId}`}>{post.node.excerpt}</p>
+       </section>
+    
+        </section>
+
+        </section>
+       
+       </section>
+       
+   
+
+    
           
       ))}
-    
+
     </>
 
   )
